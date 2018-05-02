b857d702b67c2c92bdd1231e2e0d97f628bb472b
Amos <asidelnik@gmail.com>
2018-05-02 22:27:12 +0300

    finished get json, importing class, finding searched city in json, making a city class instance object, and pushing it to cities array

--- public/script.js
+++ public/script.js
@@ -1,42 +1,77 @@
-import { City } from './city-class.js';
+import {City} from './city-class.js';
 
-let cityObj = new City("Tel Aviv", 30, 17, 12);
-console.log(cityObj.name);
+//let cityObj = new City("Tel Aviv", 30, 17, 12);
+//console.log(cityObj);
 
 
 
 let cityCards = [];
 
-var fetch = function () {
+var fetchJSON = function (cityName) {
    $.get({
       url: "cities.json",
       success: function (data) {
-         showCityWeather(data);
+         // console.log(cityName);
+         // console.log(data);
+         let searchedCity = getCityFromJSON(data, cityName);
+         pushCityToArray(searchedCity);
+         console.log(cityCards);
+         //showCityWeather(data);
       }
    });
 };
 
-function showCityWeather(data) {
-   // 0. Call global function in fetch with data parameter - worked
-   // 1. How do I connect my script file with my class file - class export & require
-   // 1.1. make an object of city class with invented parameters
-   let cityObj = new City("name", 30, "2.5.18", "19:50");
-   console.log(cityObj);
-   
-   // 1.2. draw new object parameters from function object parameter
-   let cityObj = new City(name, weather, date, time);
    // 2. add new object to my cityCards array
    // 3. Save array as string and pass to local storage
    // 4. Set array equal to parsed local storage
    // 5. Render all array objects with handlebars each
 
-
-   //console.log(data.cities[0].name);
+function getCityFromJSON(data, cityName) {
+   //let city = data.cities;
+   for (let index = 0; index < data.cities.length; index++) {
+      if(data.cities[index].name == cityName) {
+         return data.cities[index];
+      }
+   }
 }
 
-$('#search').on('click', function () {
-   fetch();
+function pushCityToArray(searchedCity) {
+   var instantiatedCity = instantiateCityObj(searchedCity);
+   cityCards.push(instantiatedCity);
+}
+
+function instantiateCityObj(searchedCity) {
+   let cityObj = new City(searchedCity.name, searchedCity.weather, searchedCity.date, searchedCity.time);
+   return cityObj;
+}
+
+$('#searchBtn').on('click', function () {
+   let $cityName = $('#searchInp').val();
+   if($cityName === "Tel Aviv" || $cityName === "Jerusalem" || $cityName === "Haifa") {
+      fetchJSON($cityName);
+   } else {
+      alert("Please choose Tel Aviv, Jerusalem, or Haifa")
+      $('#searchInp').val("");
+   }   
 });
+
+/*
+function showCityWeather(data) {
+   // 0. Call global function in fetch with data parameter - worked
+   // 1. How do I connect my script file with my class file - class export, import
+   // 1.1. make an object of city class with invented parameters - done
+   let cityObj = new City("name", 30, "2.5.18", "19:50");
+   console.log(cityObj);
+   
+   // 1.2. draw new object parameters from function object parameter
+   let cityObj = new City(name, weather, date, time);
+   
+
+
+   //console.log(data.cities[0].name);
+}*/
+
+
 // var telAviv = data.cities[0].name;
 // console.log(telAviv);
 
